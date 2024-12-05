0128c9554f184e525b54bf6c6f2254c4e5bd6880
Leonardo Cavenago <leonardocavenago05@gmail.com>
2024-12-05 17:41:05 +0100

    Day 5 - make it look better

--- 2024/day_5.py
+++ 2024/day_5.py
@@ -1,17 +1,24 @@
 from aoc import input_for
 
 
+def parse_input(puzzle_in):
+    ordering, lists = [section.splitlines() for section in puzzle_in.split('\n\n')]
+
+    ordering_rules = [(int(rule[0]), int(rule[1])) for rule in map(lambda x: x.split('|'), ordering)]
+    page_lists = [[int(x) for x in list.split(',')] for list in lists]
+
+    return page_lists, ordering_rules
+
+
 def get_rules_for(number, page_list, all_rules):
     return [r for r in all_rules if number in r and r[0] in page_list and r[1] in page_list]
 
 
 def pt_1():
-    puzzle_in = input_for(day=5, year=2024)
-    ordering, lists = [section.splitlines() for section in puzzle_in.split('\n\n')]
-    ordering_rules = [[int(part) for part in rule.split('|')] for rule in ordering]
+    page_lists, ordering_rules = parse_input(input_for(day=5, year=2024))
 
     ret = 0
-    for page_list in [list(map(int, l.split(','))) for l in lists]:
+    for page_list in page_lists:
         ordered = True
         for number in page_list:
             if not ordered:
@@ -29,16 +36,19 @@ def pt_1():
     return ret
 
 
-def sort_page_list(page_list, ordering_rules):
+def sort_page_list(page_list: list[int], ordering_rules: list[tuple[int, int]]):
     modified = False
     for number in page_list:
         rules = get_rules_for(number, page_list, ordering_rules)
 
         for rule in rules:
-            first, second = rule
-            if (fidx := page_list.index(first)) > (sidx := page_list.index(second)):
-                page_list[fidx], page_list[sidx] = page_list[sidx], page_list[fidx]
+            first_idx, second_idx = map(page_list.index, rule)
+
+            if first_idx > second_idx:
+                page_list[first_idx], page_list[first_idx] = page_list[first_idx], page_list[first_idx]
+
                 modified = True
+
     if modified:
         sort_page_list(page_list, ordering_rules)
 
@@ -46,12 +56,9 @@ def sort_page_list(page_list, ordering_rules):
 
 
 def pt_2():
-    puzzle_in = input_for(day=5, year=2024)
-    ordering, lists = [section.splitlines() for section in puzzle_in.split('\n\n')]
-    ordering_rules = [[int(part) for part in rule.split('|')] for rule in ordering]
-
+    page_lists, ordering_rules = parse_input(input_for(day=5, year=2024))
     ret = 0
-    for page_list in [list(map(int, l.split(','))) for l in lists]:
+    for page_list in page_lists:
         if sort_page_list(page_list, ordering_rules):
             ret += page_list[len(page_list) // 2]
     return ret
