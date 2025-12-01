35768bcdda532684fc56f44dc6b22e5ed8f6817f
Leonardo Cavenago <leonardocavenago05@gmail.com>
2025-12-01 20:21:44 +0100

    Tiny-ify code

--- 2025/day_01.py
+++ 2025/day_01.py
@@ -2,33 +2,24 @@ from aoc import input_for
 
 def pt_1():
     puzzle_in = input_for(day=1, year=2025)
-    count = 0
-    dial = 50
+    count, dial = 0, 50
     for rot in puzzle_in.strip().splitlines():
         step = int(rot[1:]) * (-1 if rot[0] == "L" else 1)
         dial = (dial + step) % 100
-        if dial == 0:
-            count += 1
+        count += dial == 0
     return count
 
 def pt_2():
     puzzle_in =  input_for(day=1, year=2025) # 6496
-    count = 0
-    dial = 50
+    count, dial = 0, 50
     for rot in puzzle_in.strip().splitlines():
-        step_count = int(rot[1:])
+        steps, step_count = divmod(int(rot[1:]), 100)
         step_direction = (-1 if rot[0] == "L" else 1)
-        step = (step_count % 100) * step_direction
+        step = step_count * step_direction
+        old_pos, new_pos = dial, (dial + step)
+        dial = new_pos % 100
+        count += steps + (dial == 0 or (new_pos != dial and old_pos != 0))
 
-        # Count all full rotations
-        count += step_count // 100
-
-        old_position = dial
-        new_position = (dial + step)
-        dial = new_position % 100
-
-        if dial == 0 or (new_position != dial and old_position != 0):
-            count += 1
     return count
 
 
