c4de6c300f6a45989bd2907aa073f05e066a7b16
Leonardo Cavenago <leonardocavenago05@gmail.com>
2025-12-01 22:25:50 +0100

    Day 1 One Liners

--- 2025/aoc.py
+++ 2025/aoc.py
@@ -40,17 +40,22 @@ if __name__ == "__main__":
     now = datetime.datetime.now()
     today = sys.argv[1] if len(sys.argv) == 2 else now.day
     year = now.year
-    template = """from aoc import input_for
+    template = """def pt_1(puzzle_in: str):
+    pass
 
-def pt_1():
-    puzzle_in = input_for(day={day}, year={year})
+def pt_2(puzzle_in: str):
+    pass
 
-def pt_2():
-    puzzle_in = input_for(day={day}, year={year})
+pt_1_oneline = lambda puzzle_in: ...
+pt_2_oneline = lambda puzzle_in: ...
+
 
 if __name__ == "__main__":
-    print("part one: ", pt_1())
-    print("part two: ", pt_2())"""
+    from aoc import input_for
+
+    puzzle_in = input_for(day={day}, year={year})
+    print("part one: ", pt_1(puzzle_in), pt_1_oneline(puzzle_in))
+    print("part two: ", pt_2(puzzle_in), pt_2_oneline(puzzle_in))"""
 
     dir: pathlib.Path = pathlib.Path.cwd()
     if not dir.name.isdigit():
@@ -58,8 +63,11 @@ if __name__ == "__main__":
 
     dir.mkdir(exist_ok=True, parents=True)
     file = dir / f"day_{today:02}.py"
-
-    file.write_text(template.format(day=today, year=year))
-    print("Created a new file for today!")
+    
+    if file.exists():
+        print("A file for this day already_exists.")
+    else:
+        file.write_text(template.format(day=today, year=year))
+        print("Created a new file for today!")
 
     input_for(int(today), year)
--- 2025/day_01.py
+++ 2025/day_01.py
@@ -1,7 +1,7 @@
-from aoc import input_for
+from functools import reduce
 
-def pt_1():
-    puzzle_in = input_for(day=1, year=2025)
+
+def pt_1(puzzle_in: str):
     count, dial = 0, 50
     for rot in puzzle_in.strip().splitlines():
         step = int(rot[1:]) * (-1 if rot[0] == "L" else 1)
@@ -9,20 +9,23 @@ def pt_1():
         count += dial == 0
     return count
 
-def pt_2():
-    puzzle_in =  input_for(day=1, year=2025) # 6496
+
+def pt_2(puzzle_in: str):
     count, dial = 0, 50
     for rot in puzzle_in.strip().splitlines():
         steps, step_count = divmod(int(rot[1:]), 100)
-        step_direction = (-1 if rot[0] == "L" else 1)
+        step_direction = -1 if rot[0] == "L" else 1
         step = step_count * step_direction
         old_pos, new_pos = dial, (dial + step)
         dial = new_pos % 100
         count += steps + (dial == 0 or (new_pos != dial and old_pos != 0))
-
     return count
 
+pt_1_oneline = lambda puzzle_in: reduce(lambda tup, step: (tup[0] + ((new_step:= (tup[1] + step) % 100) == 0), new_step), ((int(rot[1:]) * (-1 if rot[0] == "L" else 1)) for rot in puzzle_in.splitlines()), (0, 50))[0]  # noqa: E731
+pt_2_oneline = lambda puzzle_in: reduce(lambda tup, step: (tup[0] + (step[0] // 100) + (((new_dial:= (tup[1] + ((step[0] % 100) * step[1]))) % 100 == 0) or (tup[1] != 0 and (new_dial != (new_dial % 100)))), new_dial % 100), ((int(rot[1:]), (-1 if rot[0] == "L" else 1)) for rot in puzzle_in.strip().splitlines()), (0, 50))[0]  # noqa: E731
 
 if __name__ == "__main__":
-    print("part one: ", pt_1())
-    print("part two: ", pt_2())
+    from aoc import input_for
+    puzzle_in = input_for(day=1, year=2025).strip()
+    print("part one: ", pt_1(puzzle_in), pt_1_oneline(puzzle_in))
+    print("part two: ", pt_2(puzzle_in), pt_2_oneline(puzzle_in))
