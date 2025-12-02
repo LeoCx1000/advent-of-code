6e57bf5f241aec3a98fe0ede3edf83e7d6972614
Leonardo Cavenago <leonardocavenago05@gmail.com>
2025-12-02 13:15:20 +0100

    Day 2

---
+++ 2025/day_02.py
@@ -0,0 +1,29 @@
+import re
+
+def solve(puzzle_in, pattern):
+    total = 0
+    for id_range in puzzle_in.split(','):
+        n1, n2 = id_range.split('-')
+        for number in map(str, range(int(n1), int(n2) + 1)):
+            if pattern.fullmatch(number):
+                total += int(number)
+    return total
+
+def pt_1(puzzle_in: str):
+    return solve(puzzle_in, re.compile(r"^(\d+)\1$"))
+
+
+def pt_2(puzzle_in: str):
+    return solve(puzzle_in, re.compile(r"^(\d+)\1+$"))
+
+
+pt_1_oneline = lambda puzzle_in: sum(int(num) for r in puzzle_in.split(',') for num in map(str, range(*map(int, r.split('-'))) )if re.fullmatch(r"^(\d+)\1$", num))  # noqa: E731
+pt_2_oneline = lambda puzzle_in: sum(int(num) for r in puzzle_in.split(',') for num in map(str, range(*map(int, r.split('-'))) )if re.fullmatch(r"^(\d+)\1+$", num))  # noqa: E731
+
+
+if __name__ == "__main__":
+    from aoc import input_for
+
+    puzzle_in = input_for(day=2, year=2025)
+    print("part one: ", pt_1(puzzle_in), pt_1_oneline(puzzle_in))
+    print("part two: ", pt_2(puzzle_in), pt_2_oneline(puzzle_in))
